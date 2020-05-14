2b77a7c30c414a5353d0531cc9a2cad6c43ddf71
KokosKoko <46886807+KokosKoko@users.noreply.github.com>
2020-05-15 00:46:16 +0300

    [NEW} Added Design & Structure
    I added Dark mode/Light Mode, redesign and a toral restruction.

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-var hello = document.getElementById('article');
-hello.addEventListener('click', function(){
-    window.open('a1.html');
-    hello.style.cursor = "pointer";
-});
+        // function to set a given theme/color-scheme
+        function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.documentElement.className = themeName;
+        }
+        // function to toggle between light and dark theme
+        function toggleTheme() {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-light');
+            } else {
+                setTheme('theme-dark');
+            }
+        }
+        // Immediately invoked function to set the theme on initial load
+        (function () {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-dark');
+            } else {
+                setTheme('theme-light');
+            }
+        })();   
