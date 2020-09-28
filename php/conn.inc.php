5dc24c3d4545c3ea3cd698f52742203902ce962f
Kaloyan Doychonov <46886807+KokosTech@users.noreply.github.com>
2020-09-28 18:48:44 +0300

    Delete conn.inc.php

--- php/conn.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect('sql105.epizy.com' , 'epiz_25452004' , 'ORfRVdFqLpoN3' , 'epiz_25452004_comments');
-
-if(!$conn){
-    die("Connection failed: ".mysqli_connect_error());
-}
